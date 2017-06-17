0330a88b6fed4a0923f9fffa5c0c347454fe76d9
berdimyradov <kesha_yashok@mail.ru>
2017-06-18 02:01:19 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var CLIENT_ID = '362fe166f33a4cefa305fceaf767d009';
-var REDIRECT_URI = 'http://127.0.0.1/html/likeall.html';
+var CLIENT_ID = '782ccc18782549fd934d1e20040968f9';
+var REDIRECT_URI = 'https://like2all.github.io/html/likeall.html';
 
 $(document).ready(function () {
     var $button = $('#go_to_login_page');
